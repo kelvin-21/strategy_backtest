a806de570e12a8a6aee77c298be302161bdf9106
kelvin-21 <53601030+kelvin-21@users.noreply.github.com>
2020-11-08 15:11:00 +0800

    tune param

---
+++ const.py
@@ -0,0 +1,21 @@
+# Constants - Market Data
+DATE_TIME = 'date_time'
+OPEN = 'open'
+HIGH = 'high'
+LOW = 'low'
+CLOSE = 'close'
+ADJ_CLOSE = 'adj_close'
+VOLUME = 'volume'
+
+# Constants - Position
+BULLISH = 'bullish'
+BEARISH = 'bearish'
+
+# Constants - Strategy Events
+OPEN_POSITION = 'open_position'
+CLOSE_POSITION = 'close_position'
+NO_EVENT = 'no_event'
+
+# Technical Indicator
+TI_CCI = 'CCI'
+TI_SMA = 'SMA'
